4c80b4a2b9ad8946b5af616b4251f2681e06ea49
Ayushi <it.ayushi14@gmail.com>
2019-03-28 09:08:15 +0530

    second

--- CoreHotelRoomBookingAdminPortal/Controllers/HomeController.cs
+++ CoreHotelRoomBookingAdminPortal/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace CoreHotelRoomBookingAdminPortal.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page ishere.";
 
             return View();
         }
